a373bd48e9c75f94c46d5d29683adaf04d1710bb
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-11-11 15:30:58 +0200

    Add files via upload

--- applayout.py
+++ applayout.py
@@ -11,7 +11,7 @@ from kivymd.uix.button import MDRaisedButton,MDFlatButton, MDFillRoundFlatIconBu
 from kivymd.uix.card import MDCard
 from kivy.clock import Clock
 
-
+import librosa
 
 
 
@@ -1214,7 +1214,7 @@ class RecordForm(MDScreen):
         
         
         
-        
+        '''
         
         Fs = 8000
         n_fft = 256
@@ -1232,8 +1232,50 @@ class RecordForm(MDScreen):
         freq2 = np.fft.fftfreq(n, d=timestep)
         print("frequency2")
         print(freq2)       
+        '''
         
+        
+        # Load the example clip
+        y, sr = librosa.load(librosa.ex('nutcracker'))
 
+        # Set the hop length; at 22050 Hz, 512 samples ~= 23ms
+        hop_length = 512
+
+        # Separate harmonics and percussives into two waveforms
+        y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+        # Beat track on the percussive signal
+        tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,
+                                             sr=sr)
+
+        # Compute MFCC features from the raw signal
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+
+        # And the first-order differences (delta features)
+        mfcc_delta = librosa.feature.delta(mfcc)
+
+        #  Stack and synchronize between beat events
+        # This time, we'll use the mean value (default) instead of median
+        beat_mfcc_delta = librosa.util.sync(np.vstack([mfcc, mfcc_delta]),
+                                    beat_frames)
+
+        # Compute chroma features from the harmonic signal
+        chromagram = librosa.feature.chroma_cqt(y=y_harmonic,
+                                        sr=sr)
+
+        # Aggregate chroma features between beat events
+        # We'll use the median value of each feature between beat frames
+        beat_chroma = librosa.util.sync(chromagram,beat_frames,aggregate=np.median)
+
+        # Finally, stack all beat-synchronous features together
+        beat_features = np.vstack([beat_chroma, beat_mfcc_delta])     
+        print("beat")
+        print(beat_features)   
+        
+        
+        
+        
+        
 
         #tf.int16.max prints out as 32767 so just use this instead
         #audio_data = np.array(audio_data) / tf.int16.max
